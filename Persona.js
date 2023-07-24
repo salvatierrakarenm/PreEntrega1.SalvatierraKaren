ff0ee952be57d0f8d9f1d9f812333e3668246d35
Karen <salvatierrakarenm@gmail.com>
2023-07-24 20:28:04 -0300

    new file and modified index, main js

---
+++ Persona.js
@@ -0,0 +1,11 @@
+class Persona {
+    constructor(nombre,promedio){
+        this.nombre = nombre;
+        this.promedio = promedio;
+    }
+
+    setProm(promedio){
+        this.promedio = promedio
+    }
+
+}
--- main.js
+++ main.js
@@ -1,43 +1,62 @@
 //Programa principal.
+
 function main() {
-    alert("Bienvenidos al calculador de promedios, aqui podras promediar la edad de la cantidad de personas que quieras ingresar.");
+    alert("Bienvenidos al calculador de promedios, podras ingresar la cantidad de notas por alumnos que quieras promediar. Al finalizar dicho proceso podras ver el listado de promedios y luego, que alumno tiene el promedio mas alto.");
 
-    alert("!Atencion¡Este programa solo admite dos tipos de datos: edad para promediar (en numero) y la palabra stop para detener el proceso de ejecucion de promedios. ");
+    alert("!Atencion¡ Este programa solo admite dos tipos de datos: Las notas de cada alumno (en numero) y la palabra stop para detener el proceso de ejecucion. ");
 
-    var input = '0';
+    const alumnos = instanceStudents(); // Arreglo de alumnos instanciados.
+
     var acumulador = 0;
     var stop = false
     var cant = 0;
     var resultOperation = false;
-    let mensagge = 'El programa a finalizado. Reinicie para comenzar.'
 
-    while (!stop) {
-        var input = prompt('Ingrese una edad');
+    //Calcular Promedio de todos los alumnos.
+    alumnos.map((alumno) => {
 
-        if(input === null){
-            break;
+        while (!stop) {
+            var input = prompt(`Ingrese la nota del ${alumno.nombre}`);
+
+            if (input === null) {
+                break;
+            }
+
+            if (validateNumber(input)) {
+                acumulador = acumulador + parseInt(input);
+                cant++;
+                resultOperation = true;
+            }
+            else if (input === 'stop') {
+                stop = true;
+            } else {
+                alert('El valor ingresado no es valido')
+            }
         }
 
-        if (validateNumber(input)) {
-            acumulador = acumulador + parseInt(input);
-            cant++;
-            resultOperation = true;
-        }
-        else if(input === 'stop'){
-            stop = true;
-        }else{
-            alert('El valor ingresado no es valido')
-        }
-    }
+        stop = false;
 
-    let prom = acumulador / cant;
+        let prom = acumulador / cant;
 
-    if(resultOperation){
-        alert(`Su promedio es: ${prom}`);
-        alert(mensagge)
-    }else{
-        alert(mensagge)
-    }
+        alumno.setProm(isNaN(prom) ? 'No se ingreso una nota valida' : prom); //Setea el promedio en el alumno.
+
+    })
+
+    //Listar Alumnos.
+    var result = ''
+
+    alumnos.map((alumno) => {
+        result = result + `${alumno.nombre} Promedio: ${alumno.promedio} \n` //Mostrar los promedios
+    })
+
+    alert(result)
+
+    //Calcular el maximo promedio    
+    var maxProm = Math.max.apply(Math, alumnos.map(alumno => alumno.promedio));
+
+    const alumnMaxProm = alumnos.filter(alumno => alumno.promedio === maxProm);
+
+    alert(`El ${alumnMaxProm[0].nombre} es el mejor promedio con: ${alumnMaxProm[0].promedio}`);
 }
 
 //Funcion que valida si el parametro "input" es un numero valido.
@@ -52,13 +71,15 @@ function validateNumber(input) {
     }
 }
 
+function instanceStudents() {
+    const alumnos = [];
+    var i = 0;
+    for (i; i < 5; i++) {
+        let alumno = new Persona(`Alumno ${i}`);
+        alumnos.push(alumno)
+    }
+    return alumnos;
+}
+
 //Ejecucion del programa principal.
 main();
-
-
-
-
-
-
-
-
